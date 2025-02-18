065ad0fb4083a0694b5687d09e6308fe6c924ae4
Alicia <108745182+Shimozoto@users.noreply.github.com>
2025-02-18 22:32:27 +0100

    Add files via upload

---
+++ Rentiersim/Rentiersim/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Rentiersim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Rentiersim.Properties.Resources._88536203_rentier_läuft_weihnachten_cartoon;
+            this.pictureBox1.Location = new System.Drawing.Point(23, 216);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(91, 93);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Rentiersim.Properties.Resources._61720933_weihnachten_cartoon_rentier_läuft_entlang;
+            this.pictureBox2.Location = new System.Drawing.Point(23, 315);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(91, 94);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.button1.Location = new System.Drawing.Point(64, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 68);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Green;
+            this.panel1.Location = new System.Drawing.Point(545, 188);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(16, 243);
+            this.panel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.LightGreen;
+            this.label1.Location = new System.Drawing.Point(522, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 31);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ziel";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.button2.Location = new System.Drawing.Point(214, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 68);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Neustart";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(390, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Guthaben:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.textBox1.Location = new System.Drawing.Point(507, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(53, 30);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.textBox2.Location = new System.Drawing.Point(507, 81);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(53, 30);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(395, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Schulden:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(65, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(143, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Teilnahme = 10C";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(700, 443);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Rentierrennen";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Rentiersim/Rentiersim/Form1.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rentiersim
+{
+    public partial class Form1 : Form
+    {
+        int xPosR1, xPosR2, rentierNummer;
+        int guthaben = 10;
+        int schulden = 0;
+        Random r = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //reset
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(23, pictureBox1.Location.Y);
+            pictureBox2.Location = new Point(23, pictureBox2.Location.Y);
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //start
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = Convert.ToString(schulden);
+            textBox1.Text = Convert.ToString(guthaben);
+            if (guthaben == 0)
+            {
+                MessageBox.Show("Kein Guthaben mehr :(", "Oh oh");
+
+
+                DialogResult result = MessageBox.Show("Wollen Sie 10C leihen um zu spielen?", "Schulden?", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    schulden = schulden + 10;
+                    guthaben = guthaben + 10;
+                }
+                else
+                {
+                    MessageBox.Show("Tja dann können sie nicht spielen.", "Sicher?");
+                }
+
+            }
+            if (guthaben >= 10)
+            {
+                try
+                {
+                    rentierNummer = Convert.ToInt32(Interaction.InputBox("Welches Rentier wird gewinnen? 1 oder 2?", "Schliese eine Wette ab!"));
+                }
+                catch
+                {
+                    MessageBox.Show("Du nimmst jetzt die 2", "Pech gehabt");
+                    rentierNummer = 1;
+                }
+                
+                if (rentierNummer != 1 && rentierNummer != 2)
+                {
+                    MessageBox.Show("Falsche Eingabe nur 1 oder 2! Du nimmst jetzt die 1", "Denk nach.");
+                    rentierNummer = 1;
+                }
+                timer1.Enabled = true;
+                button1.Enabled = false;
+            }
+            else
+            {
+                return;
+            }
+
+        }
+
+        //stoppuhr
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            xPosR1 = r.Next(50);
+            xPosR2 = r.Next(50);
+            int sieger = 0;
+            pictureBox1.Location = new Point(pictureBox1.Location.X + xPosR1, pictureBox1.Location.Y);
+            pictureBox2.Location = new Point(pictureBox2.Location.X + xPosR2, pictureBox2.Location.Y);
+            if (pictureBox1.Location.X >= 545)
+            {
+                timer1.Enabled = false;
+                sieger = 1;
+            }
+            else if (pictureBox2.Location.X >= 545)
+            {
+                timer1.Enabled = false;
+                sieger = 2;
+            }
+
+            if (timer1.Enabled == false)
+            {
+                button2.Enabled = true;
+                if (sieger == rentierNummer)
+                {
+                    MessageBox.Show("Herzlichen Glückwunsch!", "Ergebnis");
+                    if (schulden <= 0)
+                    {
+                        guthaben = guthaben + 10;
+                    }
+                    else
+                    {
+                        schulden = schulden - 10;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Leider Verloren!", "Ergebnis");
+                    guthaben = guthaben - 10;
+                }
+
+            }
+            textBox2.Text = Convert.ToString(schulden);
+            textBox1.Text = Convert.ToString(guthaben);
+        }
+    }
+}
